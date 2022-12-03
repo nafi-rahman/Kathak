32488286f597a1b64b7d7a2eba3cac179bb9cde9
nafi-rahman <nafirahman16@gmail.com>
2022-12-03 01:26:34 -0700

    vercel fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import NavBar from './components/nav/NavBar'
 import Footer from './components/sections/footer'
 import { Route,Routes, Router } from 'react-router-dom'
 import Home from './components/pages/home'
-import PortfolioPage from './components/pages/portfolioPage'
 import About from './components/pages/aboutus'
 import Contact from './components/pages/contact'
+import Portfoliop from './components/pages/Portfoliop'
 function App() {
   
 
@@ -14,7 +14,7 @@ function App() {
       <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/portfolio" element={<PortfolioPage/>}/>
+        <Route path="/portfolio" element={<Portfoliop/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
       </Routes>
---
+++ src/components/pages/Portfoliop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FancyText from "../sections/FancyText";
+import Portfolio from "../portfolio/portfolio";
+
+export default function Portfoliop() {
+    return (
+        <>
+            <FancyText/>
+            <Portfolio/>
+        </>
+    );
+}
