479b557ad136a86e125d6980734120616ab7d070
nafi-rahman <nafirahman16@gmail.com>
2022-12-06 02:15:44 -0700

    gallery

--- src/components/pages/Portfoliop.jsx
+++ src/components/pages/Portfoliop.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import FancyText from "../sections/FancyText";
 import Portfolio from "../portfolio/portfolio";
+import Carousel from "../sections/Carousel";
 
 export default function Portfoliop() {
     return (
         <>
             <FancyText/>
+            <Carousel/>
             <Portfolio/>
+            
         </>
     );
 }
--- src/components/sections/Carousel.jsx
+++ src/components/sections/Carousel.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import 'tw-elements';
+import * as React from "react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Carousel(){
   
   return (
-    {}
+    <>
+    <section class="overflow-hidden bg-gray-900 text-gray-700">
+  <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+    <div class="flex flex-wrap -m-1 md:-m-2">
+      <div class="flex flex-wrap w-1/2">
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg hover:scale-105 transition duration-300 ease-in-out"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"/>
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg hover:scale-105 transition duration-300 ease-in-out"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"/>
+        </div>
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg hover:scale-105 transition duration-300 ease-in-out"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/2">
+        <div class="w-full p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg hover:scale-105 transition duration-300 ease-in-out"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"/>
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg hover:scale-105 transition duration-300 ease-in-out"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"/>
+        </div>
+        <div class="w-1/2 p-1 md:p-2">
+          <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg hover:scale-105 transition duration-300 ease-in-out"
+            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"/>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="py-10"></div>
+</section>
+
+
+
+
+
+</>
   )
   
 }
