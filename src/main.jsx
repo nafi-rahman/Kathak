ad2df1ec7135f6e04ceb2ebff8001a8240491a1a
nafi-rahman <nafirahman16@gmail.com>
2023-12-25 02:44:49 +0600

    removed slick carousel
    removed slick carousel pack to improve load times

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './style/tailwind.css'
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+
 
 import {
   BrowserRouter as Router,
